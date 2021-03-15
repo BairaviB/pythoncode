73665f36ef32df3f1fcafacbea63f0e96747038c
BairaviB <66194272+BairaviB@users.noreply.github.com>
2021-03-15 22:26:42 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-a=10
-b=20
-print(a+b)
-print("the end")
+from os import path
+from flask import Flask,request
+from pydub import AudioSegment
+
+app = Flask(__name__)
+
+@app.route('/',methods=['POST', 'GET'])
+def func():
+    if request.method == 'GET':
+        src = "sample3.mp3"
+        dst = "test11.wav"
+
+        sound = AudioSegment.from_mp3(src)
+        sound.export(dst, format="wav")
+        return "successfully converted"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+
+
